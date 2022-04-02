d8bff95e7999ba8554f37e72bff679d7bfb50c2b
jrolingdev <jroling.dev@protonmail.com>
2022-04-02 12:19:48 +1030

    Update about.svelte

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,14 +1,15 @@
 <svelte:head>
-  <title>About | Jacob Roling | Web Developer | Adelaide</title>
+	<title>About | Jacob Roling | Web Developer | Adelaide</title>
 </svelte:head>
 
 <section>
-  <h2 class="text-5xl font-bold">About</h2>
-  <p class="mt-8 text-lg">
-    Hi! 👋 My name is Jacob Roling and I'm a web developer from Adelaide, South Australia. Welcome to my portfolio. I built it using a variety of technologies.
-    For the frontend I used SvelteKit. My projects, skills and experience are
-    all data that is being pulled from Firebase. The website uses SSR so if you view the source code on the home page you
-    will notice that the projects are prerendered in the HTML. The website is styled with Tailwind. The contact form uses EmailJS to deliver contact details to my email address
-    and the site is hosted on Firebase CDN.
-  </p>
+	<h2 class="text-5xl font-bold">About</h2>
+	<p class="mt-8 text-lg">
+		Hi! 👋 My name is Jacob Roling and I'm a web developer from Adelaide, South Australia. Welcome
+		to my portfolio. I built it using a variety of technologies. For the frontend I used SvelteKit.
+		My projects, skills and experience are all data that is being pulled from Firebase. The website
+		uses SSR so if you view the source code on the home page you will notice that the projects are
+		prerendered in the HTML. The website is styled with Tailwind. The contact form uses EmailJS to
+		deliver contact details to my email address and the site is hosted on Netlify's CDN.
+	</p>
 </section>
